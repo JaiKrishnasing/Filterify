5bbb2120f5b77350cbc0d5af3b7bc1aa9b65df3d
JaiKrishnasing <150617032+JaiKrishnasing@users.noreply.github.com>
2025-02-16 21:47:16 +0100

    Comments to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,21 @@ import MainCanvas from "./components/layout/MainCanvas";
 import { ImageProvider } from "./context/ImageContext";
 import Footer from "./components/layout/Footer";
 
+// Hoofd App component
 function App() {
+  // State voor de huidige afbeelding 
   const [image, setImage] = useState(null);
 
   return (
     <ImageProvider>
       <div className="w-screen h-screen flex flex-col">
         <Header />
+          {/* Container voor het hoofdinhoudsgebied */}
         <div className="flex flex-1 overflow-hidden">
           <SideBar image={image} />
           <MainCanvas image={image} setImage={setImage} />
         </div>
+        {/* Footer component aan de onderkant */}
         <Footer image={image} />
       </div>
     </ImageProvider>

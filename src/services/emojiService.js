a19e7bc08f27ee36c33e4e017735572ce1122a11
JaiKrishnasing <150617032+JaiKrishnasing@users.noreply.github.com>
2025-02-16 21:51:28 +0100

    Comments to EmojiService

--- src/services/emojiService.js
+++ src/services/emojiService.js
@@ -1,9 +1,12 @@
-const url = "https://emojihub.yurace.pro/api/all";
+const url = "https://emojihub.yurace.pro/api/all"; // De Api
 
+// Haalt alle emoji's op van de API
 export const fetchEmojis = async () => {
   try {
     const response = await fetch(url);
 
+    // Controller of het resultaat succesvol is
+    // Zo niet? Dan een error geven
     if (!response.ok) {
       throw new Error("Network response failed");
     }
@@ -11,8 +14,11 @@ export const fetchEmojis = async () => {
     const emojiData = await response.json();
     const emojis = [];
 
+    // Iterate over de emoji data
+    // Emoji unicode omzetten naar emoji karakters 
     for (const emojiInfo of emojiData) {
       const [codePoint] = [...emojiInfo.unicode];
+      // Het unicode codepunt (bijvoorbeeld U+1F600) omzetten naar een getal
       const emojiCharacter = parseInt(codePoint.replace("U+", ""), 16);
       emojis.push(String.fromCodePoint(emojiCharacter));
     }
@@ -20,6 +26,6 @@ export const fetchEmojis = async () => {
     return emojis;
   } catch (err) {
     console.error(err);
-    return [];
+    return []; // Een lege array terug sturen indien er een fout is
   }
 };
